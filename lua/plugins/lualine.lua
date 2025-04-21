b04631813a32f876de8cbc2b8e151acaca18f8c9
gavin <gavin@vibe.us>
2025-04-21 12:33:53 +0800

    update plugins & lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,20 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require("lualine").setup()
-    end,
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+  require("lualine").setup({
+    options = {
+      icons_enabled = true,
+      theme = 'ayu_mirage',
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', colored = true, 'diagnostics'},
+        lualine_c = {'filename', path = 1 },
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+      },
+    },
+  })
+  end,
 }
